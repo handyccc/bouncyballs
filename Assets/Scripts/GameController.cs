a82886363406184d1bfe7388304321a08843150e
Andy Cochrane <andrew.cochrane@riotgames.com>
2015-12-05 23:22:55 -0800

    Initial commit of bouncy balls test app

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Note you can define several variables of the same type on one line
+	public GameObject blueBall, greenBall,redBall, orangeBall;
+	private bool isGameOver = false;
+	// Update is called once per frame
+	void Update () {
+		// If all four balls are not (!) existent then game is over
+		isGameOver = !blueBall&& !greenBall&& !redBall&& !orangeBall;
+	}
+	void OnGUI(){
+		if(isGameOver){
+			Rect rect = new Rect(Screen.width / 2 - 100,Screen.height / 2 - 50, 200, 75);
+			GUI.Box(rect, "Game Over");
+			Rect rect2 = new Rect (Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
+			GUI.Label (rect2, "Good Job!");
+		}
+	}
+}
---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider) {
+		GameObject collidedWith = collider.gameObject;
+		if (collidedWith.tag == gameObject.tag) {
+			GetComponent<Light> ().intensity = 0;
+			Destroy (collidedWith);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
